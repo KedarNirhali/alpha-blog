63cc6be5c184d6ec7aa113da47df7e46aeef0033
KedarNirhali <kedar.nirhali@gmail.com>
2021-11-14 21:53:20 +0000

    Add edit and update page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,33 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
+	end
+
+	def edit
+		#byebug
+		@article = Article.find(params[:id])
 	end
 
 	def create
 		@article = Article.new(params.require(:article).permit(:title, :description))
 		#render plain: @article.inspect
-		@article.save
-		redirect_to article_path(@article)
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		#byebug
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article was updated successfully"
+			redirect_to article_path(@article) 
+		else
+			render 'edit'
+		end
 	end
 end
