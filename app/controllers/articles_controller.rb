cc8ea93e9ed3dbb7689fd7b2f405d49b268ae950
KedarNirhali <kedar.nirhali@gmail.com>
2021-11-14 18:38:32 +0000

    Add new and create pagee

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,16 @@ class ArticlesController < ApplicationController
 	end	
 
 	def index
+		@articles = Article.all
+	end
 
+	def new
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		#render plain: @article.inspect
+		@article.save
+		redirect_to article_path(@article)
 	end
 end
